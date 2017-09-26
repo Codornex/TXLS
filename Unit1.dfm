254165eb0411696bbeab373c1d1c8a8d8e719897
Deivid <davis_43102@hotmail.com>
2017-09-26 15:25:26 -0300

    Add files via upload

---
+++ Unit1.dfm
@@ -0,0 +1,22 @@
+object Form1: TForm1
+  Left = 192
+  Top = 123
+  Width = 1088
+  Height = 563
+  Caption = 'Form1'
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'MS Sans Serif'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+  object ClientDataSet1: TClientDataSet
+    Aggregates = <>
+    Params = <>
+    Left = 520
+    Top = 248
+  end
+end
---
+++ Unit1.pas
@@ -0,0 +1,69 @@
+unit Unit1;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Classes, Graphics, Controls, Forms, Dialogs,
+  Db, DBClient;
+
+type
+  TFont = record
+    Name: String;
+    Size: Integer;
+    Bold: Boolean;
+    Sublime: Boolean;
+    Italic: Boolean;
+  end;
+
+type
+  TTitulo = record
+    Titulo: String;
+    Font: TFont;
+    Center: Boolean;
+    Upper: Boolean;
+  end;
+
+type
+  TFields = record
+    FieldName: String;
+    Caption: String;
+    CaptionF: TFont;
+  end;
+
+type
+  TProgress = record
+    ShowWindow: Boolean;
+    ShowFild: Boolean;
+  end;
+
+type
+  TXLS = record
+    TClientDataSet: TClientDataSet;
+    TCampos: Array of TFields;
+    Titulo: Array[1..3] of TTitulo;
+    AutoSave: Boolean;
+    AutoOpen: Boolean;
+    DefaultFolder: String;
+    XLSX: Boolean;
+    Progress: TProgress;
+  end;
+
+
+type
+  TForm1 = class(TForm)
+    ClientDataSet1: TClientDataSet;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.DFM}
+
+end.
+ 
